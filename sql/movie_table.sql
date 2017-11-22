ec8141fdc82944f81cead137447408d3bcf61d91
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 13:35:49 -0500

    Update movie_table.sql

--- sql/movie_table.sql
+++ sql/movie_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE movie(
+	ID int NOT NULL auto_increment,
 	title varchar(50),
 	release_year  int,
-    rating varchar(5),
-	PRIMARY KEY(title, release_year)
+	rating varchar(5),
+	PRIMARY KEY(ID)
 );
