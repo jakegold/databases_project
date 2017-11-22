ce1d50403810be1f64c32251bafc1a4bb463ec91
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 13:51:35 -0500

    Update album_table.sql

--- sql/album_table.sql
+++ sql/album_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE album(
-	  albumID int NOT NULL auto_increment,
-	  title varchar(50),
-    bandID varchar(50),
-	  PRIMARY KEY(albumID),
-    FOREIGN KEY (bandID) references band(bandID)
-		  on delete cascade
+	albumID int NOT NULL auto_increment,
+	title varchar(50),
+	bandID varchar(50),
+	PRIMARY KEY(albumID),
+	FOREIGN KEY (bandID) references band(bandID)
+		on delete cascade
 );
