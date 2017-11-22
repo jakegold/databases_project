abf38eb00276641154fd08831db4f40d59cb07e5
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 13:36:19 -0500

    Create band_table.sql

---
+++ sql/band_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE band(
+	ID int NOT NULL auto_increment,
+	title varchar(50),
+	start_playing_year  int,
+  singer varchar(50),
+  drummer varchar(50),
+  guitar_er varchar(50),
+	PRIMARY KEY(ID)
+);
