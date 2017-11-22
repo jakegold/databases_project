a50818916bb111d3cf93ee2479db37169cd87821
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 14:30:30 -0500

    Create inserts.sql

---
+++ sql/inserts.sql
@@ -0,0 +1,27 @@
+insert into user values ('jake', MD5('some'));
+insert into user values ('jeff', MD5('super'));
+insert into user values ('sam', MD5('secret'));
+insert into user values ('raizy', MD5('password'));
+
+insert into movie (title, release_year, rating) values ('Purple Rain', 1984, 'R');
+insert into movie (title, release_year, rating) values ('The Dark Knight', 2008, 'PG-13');
+insert into movie (title, release_year, rating) values ('Guardians of the Galaxy', 2014, 'PG-13');
+insert into movie (title, release_year, rating) values ('The Lord of the Rings: The Return of the King ', 2003, 'PG-13');
+
+insert into band (title) values ('The Beatles');
+insert into band (title) values ('Prince');
+insert into band (title) values ('Hans Zimmer');
+insert into band (title) values ('Blue Swede');
+insert into band (title) values ('The Raspberries');
+insert into band (title) values ('Norman Greenbaum');
+insert into band (title) values ('David Bowe');
+insert into band (title) values ('Elvin Bishop');
+insert into band (title) values ('10cc');
+insert into band (title) values ('Jackson 5');
+insert into band (title) values ('Redbone');
+insert into band (title) values ('The Runaways');
+insert into band (title) values ('Rupert Holmes');
+insert into band (title) values ('The Five Stairsteps');
+insert into band (title) values ('Marvin Gaye');
+insert into band (title) values ('Lord Of The Rings 3 Soundtrack');
+insert into band (title) values ('Howard Shore');
