5f26c9edc982cbd6696568a829236059183c3ecb
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 14:02:16 -0500

    Delete song_table.sql

--- sql/song_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE song(
-	songID int NOT NULL auto_increment,
-	title varchar(50),
-	albumID int,
-	duration int,
-	PRIMARY KEY(songID),
-	FOREIGN KEY (albumID) references album(albumID)
-		on delete cascade
-);
