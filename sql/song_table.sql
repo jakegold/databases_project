44917481cd28ad06a87821d03fe2eb16c9418235
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 14:00:28 -0500

    Create song_table.sql

---
+++ sql/song_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE song(
+	songID int NOT NULL auto_increment,
+	title varchar(50),
+    albumID int,
+    duration int,
+	PRIMARY KEY(songID),
+    FOREIGN KEY (albumID) references album(albumID)
+		on delete cascade
+);
