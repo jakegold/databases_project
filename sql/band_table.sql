b573a3bba1c8571f56b8f9f74066ea9fd4c8017d
Jake Goldstein <jsg525@nyu.edu>
2017-11-22 13:47:29 -0500

    Update band_table.sql

--- sql/band_table.sql
+++ sql/band_table.sql
@@ -1,8 +1,5 @@
 CREATE TABLE band(
-	ID int NOT NULL auto_increment,
+	bandID int NOT NULL auto_increment,
 	title varchar(50),
-    	singer varchar(50),
-	drummer varchar(50),
-    	guitar_er varchar(50),
-	PRIMARY KEY(ID)
+	PRIMARY KEY(bandID)
 );
